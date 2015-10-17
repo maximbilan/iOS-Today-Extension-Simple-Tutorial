a787848a9adb5180aa683c9ac2e2f0c9acd2f589
Maxim Bilan <maximb.mail@gmail.com>
2015-10-17 12:52:44 +0300

    Modified sample

--- TodayExtension/Extension/TodayViewController.swift
+++ TodayExtension/Extension/TodayViewController.swift
@@ -10,17 +10,21 @@ import UIKit
 import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding {
-        
+	
+	private var data: Array<NSDictionary> = Array()
+	
+	@IBOutlet weak var tableView: UITableView!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
+		
+		loadData()
+		
+		self.preferredContentSize.height = 200
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+	
+	// MARK: - NCWidgetProviding
+	
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
 
@@ -28,7 +32,51 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
 
+		loadData()
+		
         completionHandler(NCUpdateResult.NewData)
     }
-    
+	
+	func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> (UIEdgeInsets) {
+		return UIEdgeInsetsZero
+	}
+	
+	// MARK: - Loading of data
+	
+	func loadData() {
+		
+		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+			
+			self.data.removeAll()
+			
+			if let path = NSBundle.mainBundle().pathForResource("Data", ofType: "plist") {
+				if let array = NSArray(contentsOfFile: path) {
+					for item in array {
+						self.data.append(item as! NSDictionary)
+					}
+				}
+			}
+			
+			dispatch_async(dispatch_get_main_queue()) {
+				self.tableView.reloadData()
+			}
+		}
+	}
+	
+	// MARK: - TableView Data Source
+	
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return data.count
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCellWithIdentifier("tableViewCellIdentifier", forIndexPath: indexPath) 
+		
+		let item = data[indexPath.row]
+		cell.textLabel?.text = item["title"] as? String
+		cell.textLabel?.textColor = UIColor.whiteColor()
+		
+		return cell
+	}
+	
 }
